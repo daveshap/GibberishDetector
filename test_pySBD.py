b1c62ab840164ecfaf742075cd9c2dcc18e2590f
David Shapiro <daveshap37@gmail.com>
2020-11-03 13:10:59 -0500

    focusing on notebook, adding training data

--- test_pySBD.py
+++
@@ -1,8 +0,0 @@
-import spacy
-from pysbd.utils import PySBDFactory
-
-nlp = spacy.blank('en')
-nlp.add_pipe(PySBDFactory(nlp))
-
-doc = nlp('My name is Jonas E. Smith. Please turn to p. 55.')
-print(list(doc.sents))
